56b2a2f682484b97d99ac120a08d4cb73231a089
RedanHassoun <redan.has.1991@gmail.com>
2020-10-30 23:16:17 +0200

    added a unit test

--- test/users-service.spec.ts
+++ test/users-service.spec.ts
@@ -1,14 +1,20 @@
 import { AlreadyExistError } from './../exeptions/already-exist-error';
 import { UsersRepository } from './../repositories/users-repository';
 import { UsersService } from './../services/users-service';
-import { expect } from 'chai';
+import * as chai from 'chai';   
 import { User } from '../models/user';
 import { AppDBConnection } from '../repositories/app-db-connection';
 import { mock, instance, when } from 'ts-mockito';
 import { Logger } from '../common/logger';
+import { expect } from 'chai';
+import { InputError } from '../exeptions/input-error';
+
+chai.should();
+chai.use(require('chai-as-promised'));
 
 describe('Users Service', () => {
     const mockedLogger = instance(mock(Logger));
+
     it('Should throw an error in case we try to delete a user with an undefined id', () => {
         const mockedDbConnection = mock(AppDBConnection);
         const dbConnectionInstance: AppDBConnection = instance(mockedDbConnection);
@@ -52,4 +58,15 @@ describe('Users Service', () => {
         expect(usersService.getAll()).to.be.empty;
     });
 
+    it('Should throw an error if we call delete with a value that is not an integer', async () => {
+        const mockedDbConnection = mock(AppDBConnection);
+        const dbConnectionInstance: AppDBConnection = instance(mockedDbConnection);
+
+        const mockedReposetory = mock(UsersRepository);
+        const repostoryInstance: UsersRepository = instance(mockedReposetory);
+
+        const usersService = new UsersService(repostoryInstance, dbConnectionInstance, mockedLogger);
+
+        expect(usersService.delete(1.4)).to.be.eventually.rejectedWith(InputError);
+    });
 });
